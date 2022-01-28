24a186ac4e1aef7650ac7470206d9b7664d64c82
snailmail <>
2022-01-28 12:23:18 +0000

    Refreshed cache properly this time

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import logging
+
+from flask import Flask, request
+from waitress import serve
+
+from endpoints import api, api_document
+
+
+logging.basicConfig(
+    format="[%(levelname)s %(module)s] %(asctime)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    logger.info("Access to landing page")
+
+    return "Hi, I'm a landing page." # "Suck on my big fat landing page."
+
+
+@app.route("/api", methods=["POST"])
+def route_43():
+    logger.info("Access to api endpoint.")
+
+    data = request.get_json()
+    res = api.get_estimated_sentiment(data)
+
+    return res
+
+
+@app.route("/api/document", methods=["POST"])
+def route_66():
+    logger.info("Access to api/document endpoint.")
+
+    data = request.get_json()
+    res = api_document.get_estimated_sentiment(data)
+
+    return res
+
+
+serve(app, port=50055, host="0.0.0.0")
---
+++ src/models/bag_of_words.py
@@ -0,0 +1,31 @@
+import sys
+import json
+
+# hacky, don't like it
+sys.path.append("./models")
+from preprocess import clean, prep
+
+
+# read the dictionary
+with open(r"../resources/data/loughran_mcdonald_lower.json", "r") as f:
+    dictionary = json.load(f)
+
+
+def bow(lemmatized):
+    """Return the Loughran McDonald sentiment counts."""
+
+    # build the skeleton json
+    counts = {"Negative": 0, "Positive": 0, "Uncertainty": 0, "Litigious": 0}
+
+    # calculate the sentiment counts
+    for word in lemmatized:
+        if word in dictionary["Negative"]:
+            counts["Negative"] += 1
+        if word in dictionary["Positive"]:
+            counts["Positive"] += 1
+        if word in dictionary["Uncertainty"]:
+            counts["Uncertainty"] += 1
+        if word in dictionary["Litigious"]:
+            counts["Litigious"] += 1
+
+    return counts
